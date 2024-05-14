ffdb06d1402b551699ccd03d18b125260cd354d3
Alexander Cleary <alexjacleary@gmail.com>
2024-05-14 12:27:13 +1000

    Background, Button, Font, Functions

--- init.js
+++ init.js
@@ -1,1 +1,13 @@
 import {commandData} from './assets/data.js';
+
+const main = document.querySelector('main')
+const startButton = document.querySelector('#start')
+
+function displaySubjects() {}
+function displayTopics(subject) {}
+function displayFormulas(topic) {}
+function displayCommand(formula) {}
+
+startButton.addEventListener('click', () => {
+    displaySubjects()
+})
