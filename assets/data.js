9961e08f0e64ebee9c1ecd64c1d11fdd24c33235
Trishutrion <146606551+Trishutrion@users.noreply.github.com>
2024-05-16 12:36:00 +1000

    finalised database structure

--- assets/data.js
+++ assets/data.js
@@ -1,6 +1,63 @@
 export const commandData = {
-    Further: {},
-    General: {},
-    Methods: {},
-    Specialist: {}
+    "MMET": {
+        "U1&2": {
+            "Linear Relations and Equations": {
+                "Base Functions": {}, 
+                "Custom Functions" : {}
+            }, 
+            "Linear Relations and Equations": {
+                "Base Functions": {}, 
+                "Custom Functions" : {}
+            },
+            "Linear Relations and Equations": {
+                "Base Functions": {}, 
+                "Custom Functions" : {}
+            },
+            "Linear Relations and Equations": {
+                "Base Functions": {}, 
+                "Custom Functions" : {}
+            },
+            //... add more topics as required 
+        }, 
+        "U3&4": {
+            "Linear Relations and Equations": {
+                "Base Functions": {}, 
+                "Custom Functions" : {}
+            }, 
+            "Linear Relations and Equations": {
+                "Base Functions": {}, 
+                "Custom Functions" : {}
+            },
+            "Linear Relations and Equations": {
+                "Base Functions": {}, 
+                "Custom Functions" : {}
+            },
+            "Linear Relations and Equations": {
+                "Base Functions": {}, 
+                "Custom Functions" : {}
+            },
+            //... add more topics as required
+        },
+    },
+    "MSPE": {
+        "U1&2": {
+            "Linear Relations and Equations": {
+                "Base Functions": {}, 
+                "Custom Functions" : {}
+            }, 
+            "Linear Relations and Equations": {
+                "Base Functions": {}, 
+                "Custom Functions" : {}
+            },
+            "Linear Relations and Equations": {
+                "Base Functions": {}, 
+                "Custom Functions" : {}
+            },
+            "Linear Relations and Equations": {
+                "Base Functions": {}, 
+                "Custom Functions" : {}
+            },
+            //... add more topics as required
+        }, 
+    }
 }
