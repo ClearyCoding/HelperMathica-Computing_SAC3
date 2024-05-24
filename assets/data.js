f383a723a6892701e6545f6653c5599908855015
Trishutrion <146606551+Trishutrion@users.noreply.github.com>
2024-05-24 10:07:57 +1000

    finished 11mmet

--- assets/data.js
+++ assets/data.js
@@ -2,27 +2,27 @@ export const commandData = {
     "Basics": {},
     "Methods 1/2": {
         "Linear": {
-            "Solving Equations": {
+            "Equations": {
                 syntax: "Solve[{equations}, {variables}, Reals]",
                 description: "Finds the real values of the variables that makes the expression true." ,
                 example: "Input: Solve[2x + 3 == 5, x, Reals], Output: x -> 1"
             },
-            "Solving In equalities": {
+            "Inequalities": {
                 syntax: "Reduce[{inequalities}, {variables}]",
                 description: "Reduces the expression to its simplest form.",
                 example: "Input: Reduce[2x + 3 <= 5, x, Reals], Output: x <= 1"
             },
-            "Plotting Graphs": {
+            "Graphs": {
                 syntax: "Plot[{expressions}, {variable, lowerBound, upperBound}]",
                 description: "Plots the graph of the expressions over the domain and range specified.",
                 example: "Input: Plot[2x + 3, {x, -10, 10}], Output: Graph of y=2x+3 where -10<=x<=10"
             },
-            "Plotting Graphs With Parameters": {
+            "Graphs With Parameters": {
                 syntax: "Manipulate[Plot[{expressions}, {variables, lowerBound, upperBound}, PlotRange -> {lowerBound, upperBound}], {parameter, lowerBound, upperBound}] ",
                 description: "Plots the graph, provides sliders to adjust the parameters as required",
                 example: "Input: Manipulate[Plot[2x + c, {x, -10, 10}], {c, -10, 10}], Output: Graph of y = 2x + c with a slider for the values of c (between [-10, 10])"
             },
-            "Distance": {
+            "Distance formula": {
                 syntax: "EuclidianDistance[{x1, y1}, {x2, y2}]",
                 description: "Finds the distance between two points",
                 example: "Input: EuclidianDistance[{1, 2}, {3, 4}], Output: 2*Sqrt[2]"
@@ -80,99 +80,57 @@ export const commandData = {
                 description: "",
                 example: ""
             },
-            "Quotient Remainder": {
-                syntax: "",
-                description: "",
-                example: ""
-            },
-            "Plotting Graphs": {
-                syntax: "",
-                description: "",
-                example: ""
-            },
-            "Rational Root Theorem": {
-                syntax: "",
-                description: "",
-                example: ""
+            "Finding Quotient and Remiander from Long Division": {
+                syntax: "PolynomialQuotientRemainder[dividend, divisor, x]",
+                description: "Returns the quotient and the remainder when the dividend is divided by the divisor",
+                example: "Input: PolynomialQuotientRemainder[x^4 + 2 x + 1, x^2 + 1, x], Output: {-1 + x^2, 2 + 2 x}"
             }
         },
         "Inverses": {
             "Finding Inverse": {
-                syntax: "",
-                description: "",
-                example: ""
-            },
-            "POI With Inverse": {
-                syntax: "",
-                description: "",
-                example: ""
-            },
-            "Plotting Graphs": {
-                syntax: "",
-                description: "",
-                example: ""
-            }
-        },
-        "Exponentials": {
-            "Plotting Graphs": {
-                syntax: "",
-                description: "",
-                example: ""
-            },
-            "Solving Equations": {
-                syntax: "",
-                description: "",
-                example: ""
+                syntax: "Reduce[f[y]==x, y]",
+                description: "Solves the equation f(y)=x for y to find the inverse function.",
+                example: "Input: f[x_] := x^2 + 3 x + 5; Reduce[f[y] == x, y], Output: y == 1/2 (-3 - Sqrt[-11 + 4 x]) || y == 1/2 (-3 + Sqrt[-11 + 4 x])"
             }
         },
         "Trigonometry": {
-            "Solving Domain": {
-                syntax: "",
-                description: "",
-                example: ""
-            },
-            "Plotting Graphs": {
-                syntax: "",
-                description: "",
-                example: ""
+            "Solutions within the Domain": {
+                syntax: "Solve[equation && domain, x]",
+                description: "Finds all solutions to the equation within the specified domain. ",
+                example: "Input: Solve[Cos[2 x] == 0 && 0 <= x <= 2 \[Pi], x], Output: {{x -> \[Pi]/4}, {x -> (3 \[Pi])/4}, {x -> (5 \[Pi])/4}, {x -> (7 \[Pi])/4}}"
             }
         },
         "Differentials": {
             "Diffing": {
-                syntax: "",
-                description: "",
-                example: ""
+                syntax: "D[function, x]",
+                description: "Finds the derivative of the function with respect to x. ",
+                example: "Input: D[x^2,x], Output: 2x"
             },
         },
         "Applications of Differentials": {
             "Tangent Line": {
-                syntax: "",
-                description: "",
-                example: ""
+                syntax: "TangentLine[equation_, value_] := Return[Expand[Reduce[y - (equation /. x -> value) == (D[equation, x] /. x -> value) (x - value), y]] // TraditionalForm]",
+                description: "Finds the tangent line to the equation at the specified value. ",
+                example: "Input: TangentLine[x^2, 3], Output: y==6 x-9"
             },
             "Normal Line": {
-                syntax: "",
-                description: "",
-                example: ""
+                syntax: "NormaLine[equation_, value_] := Return[Expand[Reduce[y - (equation /. x -> value) == (-1/D[equation, x] /. x -> value) (x - value), y]] // TraditionalForm]",
+                description: "Finds the normal line to the equation at the specified value. ",
+                example: "Input: NormalLine[x^2, 3], Output: y==19/2-x/6"
             },
             "Stationary Points": {
-                syntax: "",
-                description: "",
-                example: ""
-            },
-            "Nature of SP": {
-                syntax: "",
-                description: "",
-                example: ""
+                syntax: "StationaryPoints[function_, x_] := Return[{x, function} /. Solve[D[function, x] == 0, x]]",
+                description: "Finds the coordinates of the stationary points of the function. ",
+                example: "Input: StationaryPoints[x^2 + 2, x], Output: {{0, 2}}"
             }
         },
         "Integrals": {
             "Integrate": {
-                syntax: "",
-                description: "",
-                example: ""
+                syntax: "Integrate[function, x]",
+                description: "Integrates the required function with respect to x. ",
+                example: "Input: Integrate[x^3,x], Output: x^4/4"
             }
-        }
+        }        
     },
     "Methods 3/4": {
         "Differentials": {
